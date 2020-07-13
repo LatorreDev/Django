e8e5e082343a5ba0423e99f19e48a69123ff508f
David Latorre <david@latorredev.com>
2020-07-12 22:35:11 -0500

    [FIX] Deleted secret key, crated environ variable

--- platzigram/platzigram/settings.py
+++ platzigram/platzigram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gdzxf@ko!my#e-7a+(gq2m5ownuik)q2u4)d*c2t_wm*^wftl_'
+SECRET_KEY = os.environ['PLATZIGRAM_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
